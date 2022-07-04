afc55bce29ae810f9c7235a28bd64428c81f9aa2
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-07-04 13:50:45 +0300

    v004

--- task015/Program.cs
+++ task015/Program.cs
@@ -10,7 +10,7 @@ int Weekend(int num)
 }
 Console.Write("Введите цифру от 1 до 7 обозначающую номер дня недели по порядку:");
 int number = Convert.ToInt32(Console.ReadLine());
-while (1 > number ^ number > 7)
+while (1 > number || number > 7)
 {
     Console.Write("Вы ввели цифру не от 1 до 7, a в недели 7 дней, попробуйте еще раз :");
     number = Convert.ToInt32(Console.ReadLine());
